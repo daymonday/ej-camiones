40c3aebc93530bb56c1c0faac5152b24456f6786
dia lunes <82815332+daymonday@users.noreply.github.com>
2024-11-28 23:30:29 -0300

    Update ej parcial transporte.c
    sigue sin andar bien pero arreglamos cuestiones de int que se querian mosotrar como float y confusiones con simbolos aritmeticos

--- ej parcial transporte.c
+++ ej parcial transporte.c
@@ -1,5 +1,5 @@
-/* La empresa Corrientes Transporta S.A.  se dedica a la prestaci´no de servicios de mensajería y paquetería.
-Desea controlar  los viajes realizados por sus 25 vehículos, para lo cual se ingresan los datos de dichos vehículos:
+/* La empresa Corrientes Transporta S.A.  se dedica a la prestaciÂ´no de servicios de mensajerÃ­a y paqueterÃ­a.
+Desea controlar  los viajes realizados por sus 25 vehÃ­culos, para lo cual se ingresan los datos de dichos vehÃ­culos:
 -Cod (int 1001 y 1025)
 -Capacidad max en kilos a transportar (real entre 100 y 10000)
 En un segundo lote de datos se ingresan por cada viaje realizado en el mes, el siguiente detalle:
@@ -10,12 +10,13 @@ Se debe rechazar el viaje cuando se supere el maximo de cada vehiculo.
 Dentro del mes un veiculo puede hacer varios viajes, uno o ninguno. La finalizacion de la carga es cod vehiculo =0.
 SE PIDE
 a) INformar la cantidad de viajes rechazados pro cada uno de los vehiculos
-b) Informar los kilos transportados por cada vehiculo con el siguiente diseño
+b) Informar los kilos transportados por cada vehiculo con el siguiente diseÃ±o
 c) Informar el codigo de vehiculo que ha realizado la manoer cantidad de viajes, dentro d los q realizaron. Informar si hay mas de uno.
 
 */
 #include <stdio.h>
 #include <stdlib.h>
+#define TAM 3
 
 int ValidarDato(int lim_inf,int lim_sup,int fin);
 float ValidarKilos(float lim_inf, float lim_sup);
@@ -28,51 +29,65 @@ void MostrarVector_Reales(float vec[],int ce,int despl);
 
 int main()
 {
-    int cod_vehiculo,cant_vehiculos=0,min_viajes=0,vec_cantViajesRechazados[25]= {0}, vec_cantViajesRealizados[25]= {0};
-    float vec_capacidadMax[25],vec_transportada[25];
+    int i,cod_vehiculo,cant_vehiculos=0,min_viajes=0,vec_cantViajesRechazados[TAM]= {0}, vec_cantViajesRealizados[TAM]= {0};
+    float mercaderia_transp=0,vec_capacidadMax[TAM],vec_transportada[TAM];
 
-    printf("\n INGRESE CODIGO DE VEHICULO [1001 A 1025, 0 PARA TERMINAR]:\n");
-    cod_vehiculo=ValidarDato(1001,1025,0);
-
-    while(cod_vehiculo!=0&& cant_vehiculos<25)
+    for(i=0; i<TAM; i++) /*cargo vector con valores*/
     {
+        printf("\n INGRESE CODIGO DE VEHICULO [1001 A 1025, 0 PARA TERMINAR]:\n");
+        cod_vehiculo=ValidarDato(1000,1025,0);
         printf("\nINGRESE CAPACIDAD DE TRANSPORTE [KILOS, numero real >100]:\n");
-        /*Max_kilos_transp=ValidarKilos(100,10000); ? se ingresan todos los valores max?*/
         vec_capacidadMax[cod_vehiculo-1001]=ValidarKilos(100,10000);
 
-        printf("\nINGRESE PESO TOTAL DE MERCADERIA TRANSPORTADA:\n");
-        vec_transportada[cod_vehiculo-1001]+=MayorA(0);
+    }
 
-        if(vec_capacidadMax[cod_vehiculo-1001]<vec_transportada[cod_vehiculo-1001])
+    printf("\n INGRESE CODIGO DE VEHICULO QUE DESEA UTILIZAR [1001 A 1025, 0 PARA TERMINAR]:\n");
+    cod_vehiculo=ValidarDato(1000,1025,0);
+
+    while(cod_vehiculo!=0)
+    {
+        /*capacidadMax[cod_vehiculo-1001]*/
+        printf("\nVEHICULO: %d \t\t CAPACIDAD DE TRANSPORTE: %.2f \n",cod_vehiculo,vec_capacidadMax[cod_vehiculo-1001]);
+
+        printf("\nINGRESE PESO TOTAL DE MERCADERIA TRANSPORTADA:\n");
+        mercaderia_transp=MayorA(0);
+
+        /* si la capacidad es menor a lo que quiero transportar, no realizo el viaje*/
+          if (mercaderia_transp <= vec_capacidadMax[cod_vehiculo - 1001]) /* no anda lo de viajes realizados??!*/
         {
-            vec_cantViajesRechazados[cod_vehiculo-1001]++;
+            vec_transportada[cod_vehiculo-1001]+=mercaderia_transp;
+            vec_cantViajesRealizados[cod_vehiculo-1001]++;
+            printf("\n%d VIAJES REALIZADOS",vec_cantViajesRealizados[cod_vehiculo-1001]);
         }
         else
         {
-            vec_cantViajesRealizados[cod_vehiculo-1001]++;
+            vec_cantViajesRechazados[cod_vehiculo-1001]++;
+            printf("\n%d VIAJES NO REALIZADOS",vec_cantViajesRechazados[cod_vehiculo-1001]);
         }
 
         cant_vehiculos++;
-        printf("\n INGRESE CODIGO DE VEHICULO [1001 A 1025, 0 PARA TERMINAR]:\n");
-        cod_vehiculo=ValidarDato(1001,1025,0);
+
+        printf("\n INGRESE CODIGO DE VEHICULO QUE DESEA UTILIZAR [1001 A 1025, 0 PARA TERMINAR]:\n");
+        cod_vehiculo=ValidarDato(1000,1025,0);
+
 
     }
 
     if(cant_vehiculos>0)
     {
-        printf("******************PUNTO A*******************************");
-        printf("\nCANTIDAD DE VIAJES RECHAZADOS POR VEHICULO\n\tVEHICULO\t\tCANT. RECHAZOS\t\n:");
-        MostrarVector(vec_cantViajesRechazados,cant_vehiculos,1001); /* aun no declaraste la funcion mostrar*/
+        printf("\n******************PUNTO A*******************************");
+        printf("\nCANTIDAD DE VIAJES RECHAZADOS POR VEHICULO\n\tVEHICULO\t\tCANT. RECHAZOS\t\n");
+        MostrarVector(vec_cantViajesRechazados,cant_vehiculos,1001);
 
 
-        printf("******************PUNTO B*******************************");
-        printf("\nCANTIDAD DE KILOS TRANSPORTADOS POR VEHICULO\n\tVEHICULO\t\tCANT. KILOS\t\n:");
-        MostrarVector_Reales(vec_transportada,cant_vehiculos,1001); /* aun no declaraste la funcion mostrar*/
+        printf("\n******************PUNTO B*******************************");
+        printf("\nCANTIDAD DE KILOS TRANSPORTADOS POR VEHICULO\n\tVEHICULO\t\tCANT. KILOS\t\n");
+        MostrarVector_Reales(vec_transportada,cant_vehiculos,1001);
 
-        printf("******************PUNTO C*******************************");
-        printf("\nCOD. VEHICULO/S CON MENOR CANTIDAD DE VIAJES\nCOD VEHICULO\t\tCANT.VIAJES\n:");
-        min_viajes=BuscoMinimo(vec_cantViajesRealizados,25);
-        BuscoIgual(vec_cantViajesRealizados,25,min_viajes,1001);
+        printf("\n******************PUNTO C*******************************");
+        printf("\nCOD. VEHICULO/S CON MENOR CANTIDAD DE VIAJES\nCOD VEHICULO\t\tCANT.VIAJES\n");
+        min_viajes=BuscoMinimo(vec_cantViajesRealizados,TAM);
+        BuscoIgual(vec_cantViajesRealizados,TAM,min_viajes,1001);
     }
 
 
@@ -80,7 +95,7 @@ int main()
     return 0;
 }
 
-int ValidarDato(int lim_inf,int lim_sup, int fin)
+int ValidarDato(int lim_inf,int lim_sup,int fin) /* el dato esta en un rango y cumple con la condicion de fin*/
 {
     int dato, band=0;
     do
@@ -99,14 +114,16 @@ int ValidarDato(int lim_inf,int lim_sup, int fin)
     return dato;
 }
 
-float ValidarKilos(float lim_inf, float lim_sup)
+float ValidarKilos(float lim_inf, float lim_sup) /* similar a lo anterior pero para float*/
 {
-    int dato,band=0;
+    int band=0;
+    float dato;
     do
     {
         if(band==0)
         {
             scanf("\n%f",&dato);
+            band=1;
         }
         else
         {
@@ -118,14 +135,15 @@ float ValidarKilos(float lim_inf, float lim_sup)
     return dato;
 }
 
-float MayorA(float lim_inf)
+float MayorA(float lim_inf) /* verifico si es un valor mayor a un limite inferior, ej mayor a 0*/
 {
     int dato,band=0;
     do
     {
         if(band==0)
         {
-            scanf("\n%d",&dato);
+            scanf("\n%f",&dato);
+            band=1;
         }
         else
         {
@@ -137,28 +155,28 @@ float MayorA(float lim_inf)
     return dato;
 }
 
-int BuscoMinimo(int vec[],int ce)
+int BuscoMinimo(int vec[],int ce) /* busco el minimo valor en u nvector*/
 {
-    int i,min,band=0;
+    int i,min=vec[0],band=0;
     for (i=0; i<ce; i++)
     {
         if(vec[i]!=0)
         {
-            if(band==0||vec[i]>min)
+            if(band==0||vec[i]<min)
                 min=vec[i];
-            band=1;
+                band=1;
         }
     }
     return min;
 }
 
-void BuscoIgual(int vec[], int ce, int nro,int despl)
+void BuscoIgual(int vec[], int ce, int nro,int despl) /* busco un valor igual al dado en otro vector*/
 {
-    int i, band=0;
+    int i;
     for (i=0; i<ce; i++)
     {
         if(vec[i] == nro)
-            printf ("\t%d", i+despl);
+            printf ("\t%d\t%d\n", i+despl, nro);
     }
 }
 
@@ -168,16 +186,16 @@ void MostrarVector(int vec[],int ce,int despl)
     for (i=0; i<ce; i++)
     {
         if(vec[i]!=0)
-            printf ("\n%d", i+despl,vec[i]);
+            printf ("\n%d\t\t%d", i+despl,vec[i]);
     }
 }
 
-void MostrarVector_Reales(float vec[],int ce,int despl)
+void MostrarVector_Reales(float vec[],int ce,int despl) /* mismo que lo anterior pero para float*/
 {
     int i;
     for (i=0; i<ce; i++)
     {
         if(vec[i]!=0)
-            printf ("\n%d", i+despl,vec[i]);
+            printf ("\n%d\t\t%.2f", i+despl,vec[i]);
     }
 }
